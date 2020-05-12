5d024d32325ce85175880877b4fadcb7150818cc
ez120 <ehsan.zand@gmail.com>
2020-05-12 18:04:25 +0430

    Updates

---
+++ precache-manifest.4f6c82fb8dc8ac1ba7792dc98773d36f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a39ac242446e22aa82595cf7ee693d",
+    "url": "/FaceDetection/index.html"
+  },
+  {
+    "revision": "bf22370674a2237309b2",
+    "url": "/FaceDetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61c697ecaeeaeb39d46a",
+    "url": "/FaceDetection/static/css/main.b462f074.chunk.css"
+  },
+  {
+    "revision": "bf22370674a2237309b2",
+    "url": "/FaceDetection/static/js/2.2b67f104.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/FaceDetection/static/js/2.2b67f104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c697ecaeeaeb39d46a",
+    "url": "/FaceDetection/static/js/main.9d1ed6ae.chunk.js"
+  },
+  {
+    "revision": "77d5849c8fde019731ed",
+    "url": "/FaceDetection/static/js/runtime-main.6fcbaf90.js"
+  }
+]);
